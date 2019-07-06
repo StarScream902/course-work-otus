f50e61ac83c7eee81a0256c7b529c3207177903c
Andrey Svezhincev <starscream902@gmail.com>
2019-07-07 00:39:31 +0300

    added APP template for packer

---
+++ packer/scripts/install_docker-compose.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
+
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+docker-compose --version
--- packer/scripts/install_docker.sh
+++ packer/scripts/install_docker.sh
@@ -1,6 +1,25 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-#Install docker
-apt update &&
-apt install -y docker
+apt-get update
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+apt-key fingerprint 0EBFCD88
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+apt-get update
+
+apt-get install -y docker-ce docker-ce-cli containerd.io
+
+docker version
