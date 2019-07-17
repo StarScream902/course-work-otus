81649c8d8deac08e21025737491942604fe1bb1d
zemond <3em0nd@gmail.com>
2019-07-17 23:36:24 +0300

    add files for app and gitlab

---
+++ terraform/gitlab/main.tf
@@ -0,0 +1,41 @@
+resource "google_compute_instance" "gitlab" {
+  name         = "${var.env}-gitlab"
+  machine_type = "n1-standard-1"
+  zone         = "${var.zone}"
+  tags         = ["gitlab"]
+
+  boot_disk {
+    initialize_params {
+      image = "${var.docker_disk_image}"
+    }
+  }
+
+  network_interface {
+    network       = "default"
+    access_config = {}
+  }
+
+  metadata {
+    ssh-keys = "r2d2:${file(var.public_key_path)}"
+  }
+
+	connection {
+    type  = "ssh"
+	  user  = "${var.ssh_user}"
+	  agent = false
+	  private_key = "${file(var.private_key_path)}"
+	}
+}
+
+# Правило firewall
+resource "google_compute_firewall" "firewall_gitlab" {
+  name    = "allow-gitlab-default"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+
+  target_tags = ["gitlab"]
+}
---
+++ terraform/modules/app/main.tf
@@ -0,0 +1,53 @@
+resource "google_compute_instance" "app" {
+  name         = "${var.env}-app"
+  machine_type = "g1-small"
+  zone         = "${var.zone}"
+  tags         = ["app","ui","crawler"]
+
+  boot_disk {
+    initialize_params {
+      image = "${var.docker_disk_image}"
+    }
+  }
+
+  network_interface {
+    network       = "default"
+    access_config = {}
+  }
+
+  metadata {
+    ssh-keys = "r2d2:${file(var.public_key_path)}"
+  }
+
+	connection {
+    type  = "ssh"
+	  user  = "${var.ssh_user}"
+	  agent = false
+	  private_key = "${file(var.private_key_path)}"
+	}
+}
+
+# Правило firewall
+resource "google_compute_firewall" "firewall_ui" {
+  name    = "allow-ui-default"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+
+  target_tags = ["ui"]
+}
+
+resource "google_compute_firewall" "firewall_crawler" {
+  name    = "allow-crawler-default"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+
+  target_tags = ["crawler"]
+}
